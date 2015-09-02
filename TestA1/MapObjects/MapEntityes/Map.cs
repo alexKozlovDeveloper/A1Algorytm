9ce6413692fae5ac1c88f3c0af5a276618c32760
Aliaksei Kazlou <aliaksei_kazlou@epam.com>
2015-09-02 16:14:41 +0300

    Init commit

---
+++ TestA1/MapObjects/MapEntityes/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MapObjects.MapEntityes
+{
+    public class Map
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Grid Grid { get; set; }
+
+        public List<List<MapCell>> Cells { get; set; }
+
+        public Map(int height, int width, Grid grid)
+        {
+            Height = height;
+            Width = width;
+
+            Grid = grid;
+        }
+
+    }
+}
---
+++ TestA1/TestA1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MapObjects.MapEntityes;
+
+namespace TestA1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Image img;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            img = new Image
+            {
+                Height = 10,
+                Width = 10,
+                Margin = new Thickness(50, 50, 0, 0),
+                Source = new BitmapImage(new Uri(@"D:\images\Green.jpg"))
+            };
+
+            GridMain.Children.Add(img);
+
+            //var map = new Map(100, 100, GridMain);
+        }
+    }
+}
